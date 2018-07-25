4476f6b3f3a66fc03c4cfa8f1e5eb317015ddfd0
llenk <ellenkeal@gmail.com>
2018-07-25 17:54:47 -0500

    added parse query

--- server/Server.java
+++ server/Server.java
@@ -3,6 +3,12 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
 import java.util.*;
+import java.net.URI;
+import java.net.URLDecoder;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 
 public class Server {
 
@@ -22,9 +28,47 @@ public class Server {
     return words;
   }
 
+  // TODO: fix so this picks a random one
   public static String pickWord(ArrayList<String> words) {
     return words.get(0);
   }
+
+  @SuppressWarnings("unchecked")
+	public static void parseQuery(String query, Map<String, Object> parameters) throws UnsupportedEncodingException {
+
+		if (query != null) {
+			String pairs[] = query.split("[&]");
+
+			for (String pair : pairs) {
+				String param[] = pair.split("[=]");
+
+				String key = null;
+				String value = null;
+				if (param.length > 0) {
+					key = URLDecoder.decode(param[0], System.getProperty("file.encoding"));
+				}
+
+				if (param.length > 1) {
+					value = URLDecoder.decode(param[1], System.getProperty("file.encoding"));
+				}
+
+				if (parameters.containsKey(key)) {
+					Object obj = parameters.get(key);
+					if (obj instanceof List<?>) {
+						List<String> values = (List<String>) obj;
+						values.add(value);
+					} else if (obj instanceof String) {
+						List<String> values = new ArrayList<String>();
+						values.add((String) obj);
+						values.add(value);
+						parameters.put(key, values);
+					}
+				} else {
+					parameters.put(key, value);
+				}
+			}
+		}
+	}
 
   public static void main(String args[]) throws IOException {
     ServerSocket server = new ServerSocket(8080);
